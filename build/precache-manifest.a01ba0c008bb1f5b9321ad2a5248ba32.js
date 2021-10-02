967abe2056f48cf742837b9098489c3efb34a20b
Brandon Parks <brandonparks2020@gmail.com>
2021-10-02 13:21:50 -0500

    build fixes

---
+++ build/precache-manifest.a01ba0c008bb1f5b9321ad2a5248ba32.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9a53adec13ff131d5a10f95cf27fd78",
+    "url": "/Pomodoro_Timer/index.html"
+  },
+  {
+    "revision": "e903be51d349af1b31e7",
+    "url": "/Pomodoro_Timer/static/css/main.cb9e985a.chunk.css"
+  },
+  {
+    "revision": "51094f3fe8fe0955c394",
+    "url": "/Pomodoro_Timer/static/js/2.51445301.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Pomodoro_Timer/static/js/2.51445301.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e903be51d349af1b31e7",
+    "url": "/Pomodoro_Timer/static/js/main.bbde1837.chunk.js"
+  },
+  {
+    "revision": "a647b2b62fdb8a5e7f8a",
+    "url": "/Pomodoro_Timer/static/js/runtime-main.7b75ae4c.js"
+  }
+]);
